79195292c9191861f7e347a5101897ab847c46e5
davis8988 <Davismarsel@gmail.com>
2020-02-22 20:28:20 +0200

    Added option to push via param to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+
+shouldPush=$1
+
 echo [Info] Building jenkins-master image
 
 echo [Info] Stopping running container: jenkins-master
@@ -13,8 +16,7 @@ docker build -t davis8988/jenkins-master .
 echo [Info] Tagging:
 docker tag davis8988/jenkins-master davis8988/jenkins-master:configured-2.194-lts 
 
-echo [Info] Pushing:
-docker push davis8988/jenkins-master:configured-2.194-lts 
+if [ -z "$shouldPush" ]; then echo [Info] Pushing:; docker push davis8988/jenkins-master:configured-2.194-lts; fi
 
 echo [Info] Removing old stopped container: jenkins-master
 docker rm -f jenkins-master
