6be1b5a4032a388a372d2de29bd14280e8079f8c
davis8988 <Davismarsel@gmail.com>
2020-02-22 20:24:07 +0200

    Added build.sh script for building & pushing container

---
+++ build.sh
@@ -0,0 +1,29 @@
+echo [Info] Building jenkins-master image
+
+echo [Info] Stopping running container: jenkins-master
+docker stop jenkins-master
+
+echo [Info] Getting new code:
+git checkout configure/users
+git pull
+ 
+echo [Info] Building image:
+docker build -t davis8988/jenkins-master . 
+
+echo [Info] Tagging:
+docker tag davis8988/jenkins-master davis8988/jenkins-master:configured-2.194-lts 
+
+echo [Info] Pushing:
+docker push davis8988/jenkins-master:configured-2.194-lts 
+
+echo [Info] Removing old stopped container: jenkins-master
+docker rm -f jenkins-master
+
+echo [Info] Running new container: jenkins-master
+export contid=$(docker run -d --name jenkins-master -p 8080:8080 davis8988/jenkins-master:configured-2.194-lts)
+
+echo [Info] Reading logs:
+docker logs -f $contid
+
+echo [Info]
+Finished
